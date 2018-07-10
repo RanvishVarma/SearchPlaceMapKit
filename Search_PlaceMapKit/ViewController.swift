e2e0354688e458659862df5256d847953626f057
Allvy <allvy@apples-macbook-pro.local>
2018-07-10 16:34:15 +0530

    Initial Commit

---
+++ Search_PlaceMapKit/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Search_PlaceMapKit
+//
+//  Created by Allvy on 7/10/18.
+//  Copyright © 2018 Allvy. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
